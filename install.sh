0d8934f9f2bc762c5f05c8bea47b13758ab8d325
hb-robo <26484694+hb-robo@users.noreply.github.com>
2025-08-31 14:08:03 -0400

    feat: add neovim config

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 echo -e "Deploying dotfiles with GNU stow..."
-stow zsh git starship tmux alacritty
+stow zsh git starship tmux alacritty nvim
 
 echo "Starting fresh shell..."
 exec zsh
